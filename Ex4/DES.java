b9369b29f72e52ae839cd48921a3beb3f2c02574
nimish <nimishsantosh107@icloud.com>
2020-09-18 13:20:38 +0530

    error removed

---
+++ Ex4/DES.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+
+class DES {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("INPUT:   ");
+		String input = scan.next();
+		System.out.println(input);
+	}
+}
